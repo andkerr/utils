d7f6ea1b81bc5f0615409441ef469f66bd1bcf81
Andrew Kerr <kerrand@protonmail.com>
2021-10-27 15:57:29 -0400

    add install script

---
+++ installutils.sh
@@ -0,0 +1,13 @@
+if [ ! -d $HOME/bin/ ]; then
+    # create a directory for custom shell scripts, and
+    # add it to the PATH env variable, if it doesn't exist
+    echo "HOME/bin does not exist, creating..."
+    mkdir $HOME/bin/
+    PATH=$PATH:$HOME/bin
+fi
+
+for file in *; do
+    if [ -x $file ] && [ $file != "installutils.sh" ]; then
+        cp $file $HOME/bin/${file%.*}
+    fi
+done
